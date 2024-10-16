1961467e2c3b9b02031a1d0319ccc12cb019c9fa
francescocifaldi <francesco.cifaldi@gmail.com>
2024-10-16 15:30:05 +0200

    ADD excercise 7 and 8

--- 01-arrays/script.js
+++ 01-arrays/script.js
@@ -45,11 +45,16 @@ console.log(teachers);
 
 // 7. Verifica se 'Fabio' è presente nell'array teachers
 // e salva il risultato nella variabile isFabioPresent
-const isFabioPresent = null;
+const isFabioPresent = teachers.includes('Fabio');
+console.log("7:");
+console.log("Fabio è presente? ",isFabioPresent);
 
 // 8. Trova l'indice dell'insegnante 'Lewis' nell'array teachers
 // e salvalo nella variabile lewisIndex
-const lewisIndex = null;
+const lewisIndex = teachers.indexOf('Lewis');
+console.log("8:");
+console.log("L'indice di Lewis è: ",lewisIndex);
+
 
 // 9. Unisci tutti gli insegnanti nell'array teachers in una stringa
 // separata da virgole e salvala nella variabile teachersString
